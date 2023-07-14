eea22f5b59fcf5658b449ddc85ae11aba75aea72
NateElison18 <122648132+NateElison18@users.noreply.github.com>
2023-07-13 18:37:27 -0600

    Finished Exercise22_01.java

--- Chapter 22 Exercises/Exercise22_01.java
+++ Chapter 22 Exercises/Exercise22_01.java
@@ -1,49 +1,52 @@
+/**
+ * Author: Nate Elison
+ * Date: 6/13/23
+ *
+ * This program takes in a string and prints a substring of the maximum consecutive increasingly ordered characters found in that string.
+ */
+
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Exercise22_01 {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		String string = "";
-		String exit = "exit";
-		
-		while (string.equalsIgnoreCase(exit) == false) {
-			System.out.print("Enter string: ");
-			string = input.next();
-			System.out.println("Maximum consecutive substring is " + getLongestSubString(string));
-		}
-	}
-	
-	public static String getLongestSubString(String string) {
-		String longestSub = "";
-		String subString = "";
-		int startIndex = 0;
-		int endIndex = 1;
-		boolean found = false;
-		
-		for (int i = 0; i < string.length() - 1; i++) {
-			if ((string.charAt(i + 1) - string.charAt(i)) == 1) {
-				endIndex = (i + 2);
-				if (endIndex > string.length() - 1) {
-					endIndex = string.length();
-				}
-				subString = string.substring(startIndex, endIndex);
-			}
-			else {
-				if (found) {
-					subString = string.substring(startIndex, endIndex);
-					startIndex = i + 1;
-				}
-				else {
-					return "no substring found";
-				}	
-			}
-			if (subString.length() > longestSub.length()) {
-				longestSub = subString;
-			}
-		}
-		return longestSub;
-	}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String string = "";
+        String exit = "exit";
+
+        while (string.equalsIgnoreCase(exit) == false) {
+            System.out.print("Enter string (enter exit to quit program): ");
+            string = input.next();
+            System.out.println("Maximum consecutive substring is " + getLongestSubString(string));
+        }
+    }
+
+    public static String getLongestSubString(String string) {
+        String longestSub = "";
+        String subString;
+        int startIndex = 0;
+        int endIndex = 1;
+
+        for (int i = 0; i < string.length() - 1; i++) {
+            if ((string.charAt(i + 1) - string.charAt(i)) == 1) {
+                endIndex = (i + 2);
+                if (endIndex > string.length() - 1)
+                    endIndex = string.length();
+                subString = string.substring(startIndex, endIndex);
+            }
+            else {
+                subString = string.substring(startIndex, endIndex);
+                startIndex = i + 1;
+                endIndex++;
+            }
+            if (subString.length() > longestSub.length())
+                longestSub = subString;
+
+        }
+        if (longestSub.length() == 1)
+            return "no consecutive characters found";
+        else
+            return longestSub;
+    }
 
 }
